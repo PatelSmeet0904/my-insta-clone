219c204cab067ffa79f6251616d6fb42b47339a8
smeetpatel0904 <smeet.patel@lucentinnovation.com>
2023-05-08 16:39:57 +0530

    insta-clone

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,181 @@
+import React, { useContext, useRef, useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { UserContext } from "../App";
+import M from "materialize-css";
+import "./Navbar.css";
+const NavBar = () => {
+  const searchModal = useRef(null);
+  const [search, setSearch] = useState("");
+  const [userDetails, setUserDetails] = useState([]);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    M.Modal.init(searchModal.current);
+  }, []);
+
+  useEffect(() => {
+    // Close the navbar on route change
+    setIsNavOpen(false);
+  }, [history.location.pathname]);
+
+  const handleNavToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  const renderList = () => {
+    if (state) {
+      return [
+        <li key="2">
+          <Link to="/profile">Profile</Link>
+        </li>,
+        <li key="3">
+          <Link to="/create">Create Post</Link>
+        </li>,
+        <li key="4">
+          <Link to="/myfollowingpost">My following Posts</Link>
+        </li>,
+        <li key="5">
+          <button
+            className="btn #c62828 red darken-3"
+            onClick={() => {
+              localStorage.clear();
+              dispatch({ type: "CLEAR" });
+              history.push("/signin");
+            }}
+          >
+            Logout
+          </button>
+        </li>,
+      ];
+    } else {
+      return [
+        <li key="6">
+          <Link to="/signin">Sign-in</Link>
+        </li>,
+        <li key="7">
+          <Link to="/signup">Sign-up</Link>
+        </li>,
+      ];
+    }
+  };
+
+  const fetchUsers = (query) => {
+    setSearch(query);
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/search-users`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query,
+      }),
+    })
+      .then((res) => res.json())
+      .then((results) => {
+        setUserDetails(results.user);
+      });
+  };
+  return (
+    <nav>
+      <div className="nav-wrapper white">
+        <Link to={state ? "/" : "/signin"} className="brand-logo left">
+          My-Insta
+        </Link>
+        <Link
+          to="/"
+          data-target="mobile-demo"
+          className={`sidenav-trigger right ${isNavOpen ? "active" : ""}`}
+          onClick={handleNavToggle}
+        >
+          <i className="material-icons">menu</i>
+        </Link>
+        <ul
+          id="nav-mobile"
+          className={`right hide-on-med-and-down ${
+            isNavOpen ? "sidenav-open" : ""
+          }`}
+        >
+          <li key="1">
+            <i
+              data-target="modal1"
+              className="large material-icons modal-trigger"
+              style={{ color: "black" }}
+            >
+              search
+            </i>
+          </li>
+          {renderList()}
+        </ul>
+      </div>
+      <ul
+        id="mobile-demo"
+        className={`sidenav ${isNavOpen ? "sidenav-open" : ""}`}
+      >
+        <div className="fix-style">
+          <li key="1">
+            <i
+              data-target="modal1"
+              className="large material-icons modal-trigger"
+              style={{ color: "black" }}
+            >
+              search
+            </i>
+          </li>,
+          <li className="close-button">
+            <button
+              className="modal-close waves-effect waves-green btn-flat"
+              onClick={handleNavToggle}
+            >
+              <i className="material-icons">close</i>
+            </button>
+          </li>
+        </div>
+        {renderList()}
+      </ul>
+      <div
+        id="modal1"
+        className="modal"
+        ref={searchModal}
+        style={{ color: "black" }}
+      >
+        <div className="modal-content">
+          <input
+            type="text"
+            placeholder="search users"
+            value={search}
+            onChange={(e) => fetchUsers(e.target.value)}
+          />
+          <ul className="collection">
+            {userDetails.map((item) => {
+              return (
+                <Link
+                  to={
+                    item._id !== state._id ? "/profile/" + item._id : "/profile"
+                  }
+                  onClick={() => {
+                    M.Modal.getInstance(searchModal.current).close();
+                    setSearch("");
+                  }}
+                >
+                  <li className="collection-item">{item.email}</li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="modal-footer">
+          <button
+            className="modal-close waves-effect waves-green btn-flat"
+            onClick={() => setSearch("")}
+          >
+            close
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
