242e809f26408f1fa5935f2962b0938601f7c859
smeetpatel0904 <smeet.patel@lucentinnovation.com>
2023-05-08 17:06:52 +0530

    insta-clone-update

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,4 @@
 require("dotenv").config()
-
 module.exports={
     MONGOURI:`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.tyzulp1.mongodb.net/instagram-clone`,
     JWT_SECRET:process.env.JWT_SEC,
